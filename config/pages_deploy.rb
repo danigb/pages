dea108ee3d6d57d4424eba445c9bf865b4283c71
dani <dani@mandarine.(none)>
2008-07-24 19:26:46 +0200

    old app config removed

--- config/pages_deploy.rb
+++
@@ -1,7 +0,0 @@
-
-#ENV['INLINEDIR'] = File.join(File.dirname(__FILE__),"../../inline")
-APP_CONFIG = {:files_path => 'public/files/development', 
-  :root => {:controller => 'si/ver', :action => 'index'},
-  :edit => {:controller => 'si/cambiar', :action => 'index'},
-  :user => {:name => 'solidaridad', :pass => 'solidaridad'}
-}
