bf00a76caf9bb29b980490838ff0bb4d8db1dbdc
Dani Blasco Gomez <dani@pivot.toami.net>
2008-07-24 19:25:32 +0200

    current

--- config/pages_deploy.rb
+++ config/pages_deploy.rb
@@ -1,5 +1,7 @@
 
-#ENV['INLINEDIR'] = File.join(File.dirname(__FILE__),"../../inline")
-$pages = {:files_path => 'public/files/development', 
-  :root => {:controller => 'si/ver', :action => 'index'}
+ENV['INLINEDIR'] = File.join(File.dirname(__FILE__),"../../inline")
+$pages = {:files_path => 'public/files', 
+  :root => {:controller => 'si/ver', :action => 'index'},
+  :edit => {:controller => 'si/cambiar', :action => 'index'},
+  :user => {:name => 'solidaridad', :pass => 'solidaridad'}
 }
