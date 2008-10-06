0e4eca3ca03a7bb9609b3df913155a5447da32cf
dani <dani@mandarine.(none)>
2008-10-06 20:35:35 +0200

    y algunas refactorizaciones

--- config/routes/routes_xy.rb
+++
@@ -1,6 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.root :controller => 'xy/ver'
-  map.connect '/ver/:id', :controller => 'xy/ver', :action => 'pagina'
-  map.connect ':action', :controller => 'xy/ver'
-end
-
