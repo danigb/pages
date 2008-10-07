963aa1c5b40fb5157889f2ac54deefd568265adb
dani <dani@mandarine.(none)>
2008-10-07 17:10:47 +0200

    calcaxy fixes

--- config/applications/routes_xy.rb
+++ config/applications/routes_xy.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   AdminComponent.route(map)
-  map.root :controller => 'xy/ver'
+  map.root :controller => 'xy/calcaxy'
   map.connect '/ver/:id', :controller => 'xy/calcaxy', :action => 'pagina'
   map.connect ':action', :controller => 'xy/calcaxy'
 end
