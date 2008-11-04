19ab3e0164e6bc24ad50cad3844276b85eee03a1
dani <dani@mandarine.(none)>
2008-11-04 20:08:08 +0100

    mudanza

---
+++ public/javascripts/xy/whatiscalc.js
@@ -0,0 +1,14 @@
+
+(function($) {
+    $(function() {
+        $("#cword").focus();
+        $("form").submit(function() {
+            console.log("submit");
+        });
+    })
+
+    isValid = function(text) {
+        return false;
+    };
+})(jQuery);
+
